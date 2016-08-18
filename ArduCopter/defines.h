5c0717cffbbe810b7ad743d9aa56f4acad7d7d64
martinscrach <m.vigneau@net.estia.fr>
2016-08-18 11:13:28 +0200

    Update defines.h

--- ArduCopter/defines.h
+++ ArduCopter/defines.h
@@ -103,10 +103,10 @@ enum control_mode_t {
     FLIP =         14,  // automatically flip the vehicle on the roll axis
     AUTOTUNE =     15,  // automatically tune the vehicle's roll and pitch gains
     POSHOLD =      16,  // automatic position hold with manual override, with automatic throttle
-    BRAKE =        20,  // full-brake using inertial/GPS system, no pilot input
+    NEWFLIGHTMODE = 17, // new flight mode description
     THROW =        18,  // throw to launch mode using inertial/GPS system, no pilot input
     AVOID_ADSB =   19,  // automatic avoidance of obstacles in the macro scale - e.g. full-sized aircraft
-    NEWFLIGHTMODE = 17, // new flight mode description
+    BRAKE =        20,  // full-brake using inertial/GPS system, no pilot input
 };
 
 enum mode_reason_t {
